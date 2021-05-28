91cbaf70d4db873065969f07996805afbc1a6c05
Jagjeet-Ramgharia <ramghariajagjeet4281@gmail.com>
2021-05-28 19:33:46 +0530

    test

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Services from './Services';
+import Navbar from './Navbar';
+import {Switch , Route, Redirect} from 'react-router-dom';
+
+
+const App = () =>{
+  return(
+    <>
+    <Navbar/>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/contact" component={Contact}/>
+      <Route exact path="/projects" component={Services}/>
+      <Redirect to="/"/>
+    </Switch>
+    
+    </>
+  )
+}
+export default App;
